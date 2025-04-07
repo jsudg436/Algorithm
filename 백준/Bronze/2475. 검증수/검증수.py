84d0c74ee65a1840ed69411189b2294efa79b200
jsudg436 <73703149+jsudg436@users.noreply.github.com>
2025-04-07 13:43:17 +0900

    Update 검증수.py

--- 백준/Bronze/2475. 검증수/검증수.py
+++ 백준/Bronze/2475. 검증수/검증수.py
@@ -1,9 +1,10 @@
 import sys
-
+#정수형으로 입력받기 위해 map 함수 사용
 num = list(map(int, sys.stdin.readline().split()))
+#좀 더 빠른 입력을 위해 input이 아닌 sys.stdin.readline()을 사용
 sum = 0
-
+#입력받은 수들을 제곱하여 sum에 추가
 for i in num:
     sum += i**2
-
+#sum을 10으로 나눈 나머지인 검증수를 출력
 print(sum % 10)
