ee90191708ee86ab67eec53080ed72b6f50ffa33
jsudg436 <73703149+jsudg436@users.noreply.github.com>
2025-04-07 13:48:27 +0900

    Update 별 찍기 － 7.py

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -1,6 +1,7 @@
 import sys
 N = int(sys.stdin.readline())
 
+#범위를 -N+1 부터 N-1까지 돌게하면서 abs 함수를 같이 사용해 증감하는 패턴을 절댓값을 사용해 하나의 반복문으로 표현
 for i in range(-N + 1, N):
     print(' ' * abs(i) + '*' * (2 * (N - abs(i)) - 1))
 
